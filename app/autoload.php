ff4ae2425716e113bd59a03acb6710d638d13353
everzet <ever.zet@gmail.com>
2012-01-03 17:25:06 +0100

    removed UniversalClassLoader as it's not needed anymore

--- app/autoload.php
+++ app/autoload.php
@@ -1,17 +1,13 @@
 <?php
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require_once __DIR__.'/../vendor/.composer/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    $universalLoader = new UniversalClassLoader();
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $universalLoader->registerPrefixFallbacks(array(__DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs'));
-    $universalLoader->register();
+    $loader->add(null, __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 AnnotationRegistry::registerLoader(function($class) use ($loader) {
